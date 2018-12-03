01ce7fdcee58a9c8b4130c8c1de1fac6bec40398
Makar Soloshich <msoloshich@gmail.com>
2018-12-03 18:44:37 +0400

    Add captcha check. Spring session and rememberMe.

--- src/main/java/com/example/textBox/controller/RegistrationController.java
+++ src/main/java/com/example/textBox/controller/RegistrationController.java
@@ -1,8 +1,11 @@
 package com.example.textBox.controller;
 
 import com.example.textBox.domain.User;
+import com.example.textBox.domain.dto.CaptchaResponseDto;
 import com.example.textBox.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -11,14 +14,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+    private static final String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
     @Autowired
     private UserService userService;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/registration")
     public String registration() {
         return "registration";
@@ -27,10 +41,18 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(
             @RequestParam("password2") String passwordConfirm,
+            @RequestParam("g-recaptcha-response") String captchaResponse,
             @Valid User user,
             BindingResult bindingResult,
             Model model
     ) {
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+       CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+       if(!response.isSuccess()) {
+           model.addAttribute("captchaError", "Fill captcha");
+       }
+
         boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
         if(isConfirmEmpty) {
             model.addAttribute("password2Error", "Password confirmation cannot be empty");
@@ -40,7 +62,7 @@ public class RegistrationController {
             model.addAttribute("passwordError", "Password are different!");
         }
 
-        if(isConfirmEmpty || bindingResult.hasErrors()) {
+        if(isConfirmEmpty || bindingResult.hasErrors() || !response.isSuccess()) {
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
